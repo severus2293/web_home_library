3d04431a7dafcb985c74569810057da2bcc56d09
severus2293 <sologub321@mail.ru>
2022-10-07 16:32:59 +0300

    restart

--- public/script_files/index.js
+++
@@ -1,8 +0,0 @@
-
-function correct(){
-    if(document.getElementById("username").value.trim() ===""){
-        document.getElementById("err").style.display = "block"
-        return false;
-    }
-    return true;
-}
